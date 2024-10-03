d6c601565db00dfdd90b76cab3fce1b52c08fe34
Shahram <shahramdeveloper11@gmail.com>
2024-10-03 11:03:41 +0330

    debugShowCheckerModeBanner value set false in main.dart in MaterialApp.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vazir'),
       home: Scaffold(
         body: SafeArea(child: Text('شهرام آکادمی')),
