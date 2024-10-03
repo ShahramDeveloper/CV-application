11101a19b7a291a507f2a641963d5b4263a2e665
Shahram <shahramdeveloper11@gmail.com>
2024-10-03 10:42:33 +0330

    MaterialApp create in Application class in main.dart. vazir font family set to theme data in MaterialApp.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,11 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'vazir'),
+      home: Scaffold(
+        body: SafeArea(child: Text('شهرام آکادمی')),
+      ),
+    );
   }
 }
